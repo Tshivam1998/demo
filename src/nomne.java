f4e364b07919ccc0617f7602afb61259c8e060c8
Shivam <tshivam@telaverge.com>
2022-11-05 13:03:00 +0530

    adding new fearture for the same

--- src/nomne.java
+++ src/nomne.java
@@ -1,6 +1,10 @@
 public class nomne {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hey welcome to the independent programming languwage java");
+
+
+        System.out.println("welcome to the new one version of the code");
     }
+
+
 }
