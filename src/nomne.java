c4b60338d2e4f5e855b3b6f7019b4c658a186158
Shivam <tshivam@telaverge.com>
2022-11-05 12:59:43 +0530

    small

--- src/nomne.java
+++ src/nomne.java
@@ -2,5 +2,6 @@ public class nomne {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hey welcome to the independent programming languwage java");
+        System.out.println("le small changes");
     }
 }
