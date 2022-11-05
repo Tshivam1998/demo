7037234553a0d481b14d7d8bc21e416820964aed
Shivam <tshivam@telaverge.com>
2022-11-05 08:21:41 +0530

    small changes

---
+++ src/nomne.java
@@ -0,0 +1,6 @@
+public class nomne {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hey welcome to the independent programming languwage java");
+    }
+}
